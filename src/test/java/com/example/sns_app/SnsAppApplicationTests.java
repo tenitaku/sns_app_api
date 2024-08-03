60e8674054bcd7d5796bdb9694a1453aca1b08dc
Fukushima Takumi <fukushimatakumi@FukushimanoMacBook-Pro.local>
2024-08-03 18:29:57 +0900

    Initial commit

---
+++ src/test/java/com/example/sns_app/SnsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sns_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnsAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
